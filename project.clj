b090244657e31ce9a7d971e3af9e5bfcf03e4e5b
KPCCoiL <copernicium@me.com>
2019-05-03 16:33:39 +0900

    Edit readme, project.clj

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
-(defproject figwheel-main/lein-template "0.1.9-3"
-  :description "A Leinigen template for figwheel"
-  :url "https://github.com/bhauman/figwheel-main-template"
+(defproject tsglive-game-template "0.0.1"
+  :description "A Leiningen template for TSG Live! live game programming"
+  :url "https://github.com/KPCCoiL/tsglive-game-template"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :scm { :name "git"
-         :url "https://github.com/bhauman/figwheel-main-template"}
+         :url "https://github.com/KPCCoiL/tsglive-game-template"}
   :eval-in-leiningen true)
